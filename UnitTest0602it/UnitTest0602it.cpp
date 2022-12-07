2116a4c68855d193861febb14dd0ba6cd9e48092
zhabgraham <115187364+zhabgraham@users.noreply.github.com>
2022-12-07 15:23:48 +0300

    Add files via upload

---
+++ UnitTest0602it/UnitTest0602it.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_02it/Lab_06_02it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+int Test(int* r, int*c, const int n)
+{
+	for (int i = 0; i < 2 * n; i++)
+	{
+		Sort(r, 3, i);
+		return r[i];
+	}
+	for (int i = 0; i < 2*n; i++)
+		Assert::AreEqual(r[i], c[i]);
+}
+
+namespace UnitTest0602it
+{
+	TEST_CLASS(UnitTest0602it)
+	{
+	public:
+	          TEST_METHOD(TestMethod1)
+			{
+				  const int n = 3;
+				  int i = 0;
+				  int r[2*n] = {2, 3, 4, 5, 6, 7};
+				  int c[2*n]= { 5, 6, 7, 2, 3, 4 };
+				  Test(r, c, n);
+		    }
+	};
+}
